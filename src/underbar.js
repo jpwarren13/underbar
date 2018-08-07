5dae938ecaa7813f326405eae12e74b5cec039c4
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-08-07 09:16:27 -0700

    filter working

--- src/underbar.js
+++ src/underbar.js
@@ -53,6 +53,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      } }else {
+        for (var i in collection) {
+	iterator(collection[i], i, collection);
+	}
+      }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -74,6 +83,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+  var passed = [];
+    _.each(collection, function(item) {
+      if (test(item)){
+	      passed.push(item);
+      }
+    });
+  return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
